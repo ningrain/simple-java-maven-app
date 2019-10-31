6c51720737b62a3374a0ec2020485871a46c9a1d
jiangningning <jiangningning@haohandata.com.cn>
2019-10-31 15:44:24 +0800

    update

--- jenkins/scripts/refactory.sh
+++ jenkins/scripts/refactory.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pwd
-cp /usr/local/jenkins-data/workspace/simple-java-maven-app/target/my-app-1.0-SNAPSHOT.jar /root
+pwd 
+cp /usr/local/jenkins-data/workspace/simple-java-maven-app/target/my-app-1.0-SNAPSHOT.jar /root 
