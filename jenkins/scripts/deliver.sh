845ff924181e13e0a623241ac5c48b95e071abf2
ningrain <30428414+ningrain@users.noreply.github.com>
2019-10-31 15:30:00 +0800

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -23,6 +23,7 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
+cp ${NAME}-${VERSION}.jar ~
 java -jar target/${NAME}-${VERSION}.jar
 
-cp ${NAME}-${VERSION}.jar ~
+
